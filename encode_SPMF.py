f9c37ae93421c7afcd52d8a14a58b065cb679fd4
Cécile Macaire <cecile.macaire@live.com>
2021-01-30 23:11:24 +0100

    modif encode + commentaires

--- encode_SPMF.py
+++ encode_SPMF.py
@@ -12,8 +12,8 @@ import sys
 
 def encode_file(name_file):
     # éliminer les colonnes des variables non utilisées
-    list_2_drop = ['REGION', 'ACHLR', 'AEMM', 'AEMMR', 'AGED', 'AGEREV', 'AGEREVQ', 'ANAI', 'ANARR', 'ANEM', 'ANEMR',
-                   'APAF', 'ASCEN',
+    list_2_drop = ['ACHLR', 'AEMM', 'AEMMR', 'AGED', 'AGEREV', 'AGEREVQ', 'ANAI', 'ANARR', 'ANEM', 'ANEMR',
+                   'APAF', 'ASCEN','REGION', 
                    'BAIN',
                    'CATL', 'CATPC', 'CHAU', 'CHFL', 'CHOS', 'CLIM', 'CMBL', 'COUPLE', 'CS2', 'CS3', 'CUIS',
                    'DEPT', 'DEROU',
@@ -31,19 +31,19 @@ def encode_file(name_file):
     # Lire le fichier entrée et le convertir en csv
     df = pd.read_csv(name_file, sep=';', low_memory=False)
     df_csv = df.drop(list_2_drop, axis=1)  # on élimine les colonnes non gardées
-    txt_filter = df_csv.to_csv(r'GrandEst_filter.txt', index=None, sep=' ', mode='a')
+    txt_filter = df_csv.to_csv('GrandEst_filter.txt', index=None, sep=' ', mode='w')
     # Construction du fichier de données encodées
     dict_encode = {}
     indice = 1
     with open('GrandEst_filter.txt', 'r') as f:
         lines = f.readlines()
         values = []
-        header = lines[0].split(' ')
+        header = lines[0].split(' ')  # récupère le nom des attributs
         for l in lines[1:]:
-            el = l.split(' ')
+            el = l.split(' ')  # on récupère les attributs dans une liste
             enc_l = []
-            for i, j in enumerate(el):
-                enc = str(header[i]) + '_' + str(j)
+            for i, j in enumerate(el):  # construction du dictionnaire pour le mapping
+                enc = str(header[i]) + '_' + str(j)  # avec le nom de l'attribut et sa valeur
                 if enc not in dict_encode.keys():
                     dict_encode[enc] = indice
                     indice += 1
@@ -53,13 +53,13 @@ def encode_file(name_file):
             enc_l.sort()
             values.append(enc_l)
         print(dict_encode)
-    with open('GrandEst_encode.txt', 'w+') as ff:
+    with open('GrandEst_encode.txt', 'w+') as ff:  # on crée le fichier encodé
         for a in values:
             l2 = [str(i) for i in a]
             ff.write(" ".join(l2))
             ff.write("\n")
     # Enregistrer le dictionnaire d'encodage
-    with open('dictionnaire.pickle', 'wb') as handle:
+    with open('dictionnaire.pickle', 'wb') as handle:  # on sauvegarde le dictionnaire de l'encodage
         pickle.dump(dict_encode, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
