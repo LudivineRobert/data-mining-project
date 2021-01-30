85e1ce6ff107104c399cb232ff04b98d07c62fe6
Cécile Macaire <cecile.macaire@live.com>
2021-01-30 14:46:28 +0100

    lignes de commandes spmf itemsets et association rules

--- commandes_AssRules_SPMF.sh
+++ commandes_AssRules_SPMF.sh
@@ -5,9 +5,9 @@
 java -jar spmf.jar run FPGrowth_association_rules GrandEst_encode.txt output_asr_sup80_conf60.txt 80% 60%
 
 # minsup 80% minconf 80%
-java -jar spmf.jar run FPGrowth_association_rules GrandEst_encoded_for_SPMF.txt output_asr_sup80_conf80.txt 80% 80%
+java -jar spmf.jar run FPGrowth_association_rules GrandEst_encode.txt output_asr_sup80_conf80.txt 80% 80%
 
 # minsup 80% minconf 90%
-java -jar spmf.jar run FPGrowth_association_rules GrandEst_encoded_for_SPMF.txt output_asr_sup80_conf90.txt 80% 90%
+java -jar spmf.jar run FPGrowth_association_rules GrandEst_encode.txt output_asr_sup80_conf90.txt 80% 90%
 
 
